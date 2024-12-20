25ca7e9503f7fc28cb084c92fe3fc2a1b7add30b
Daniyar Yeralin <daniyar.yeralin@gmail.com>
2024-12-20 17:39:31 -0500

    Bring back mp3 streaming

--- streamers/spotify.py
+++ streamers/spotify.py
@@ -2,11 +2,14 @@
 import re
 from io import BytesIO
 from os import getenv
+import subprocess
+import threading
 from typing import Generator, List, Optional, Tuple
 
 import spotipy
 from librespot.audio import PlayableContentFeeder
 from librespot.audio.decoders import AudioQuality, VorbisOnlyAudioQuality
+from librespot.proto.Metadata_pb2 import AudioFile
 from librespot.core import Session
 from librespot.metadata import TrackId
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -53,6 +56,29 @@ class SpotifyStreamer(BaseStreamer):
     def get_name(self) -> str:
         return "Spotify"
 
+    def _extract_bitrate(self, audio_format: AudioFile.Format) -> int:
+        if audio_format in [
+                AudioFile.MP3_96,
+                AudioFile.OGG_VORBIS_96,
+                AudioFile.AAC_24_NORM,
+        ]:
+            return 96
+        if audio_format in [
+                AudioFile.MP3_160,
+                AudioFile.MP3_160_ENC,
+                AudioFile.OGG_VORBIS_160,
+                AudioFile.AAC_24,
+        ]:
+            return 160
+        if audio_format in [
+                AudioFile.MP3_320,
+                AudioFile.MP3_256,
+                AudioFile.OGG_VORBIS_320,
+                AudioFile.AAC_48,
+        ]:
+            return 320
+        raise RuntimeError("Unknown format: {}".format(format))
+
     def search(self, query: str, limit: int = 20) -> List[Entry]:
         results = []
         search_output = self.spotify_api.search(q=query, limit=limit)
@@ -62,7 +88,7 @@ class SpotifyStreamer(BaseStreamer):
             title = " - ".join([artists, item["name"]])
             # Construct url
             track_id = self.parse_spotify_track_id(item["uri"])
-            url = utils.construct_url(self.stream_path, scheme="https", trackId=track_id)
+            url = utils.construct_url(self.stream_path, scheme="http", trackId=track_id)
             results.append(Entry(title, url, self.get_name()))
         return results
 
@@ -78,15 +104,22 @@ class SpotifyStreamer(BaseStreamer):
         playable_content = self.content_feeder.load(
             TrackId.from_uri("spotify:track:" + spotify_track_id), preferred_quality, False, None
         )
+        preferred_file = preferred_quality.get_file(
+            playable_content.track.file)
         # Get metadata
         artists = ", ".join([artist.name for artist in playable_content.track.artist])
         title = " - ".join([artists, playable_content.track.name])
         duration = playable_content.track.duration // 1000  # ms to sec
+        bitrate = self._extract_bitrate(preferred_file.format)
+        size = utils.estimate_size(duration, bitrate)
         # Generate stream
-        stream = self.generate_stream(playable_content, range_start, range_end)
-        return (stream, title, duration, playable_content.input_stream.size)
+        stream = self.generate_stream(playable_content, bitrate, size)
+        # Ogg stream
+        # stream = self.generate_stream_ogg(playable_content, range_start, range_end)
+        # return (stream, title, duration, playable_content.input_stream.size)
+        return (stream, title, duration, size)
 
-    def generate_stream(
+    def generate_stream_ogg(
         self,
         content: PlayableContentFeeder.LoadedStream,
         range_start: int,
@@ -109,4 +142,49 @@ class SpotifyStreamer(BaseStreamer):
                 start_chunk += 1
                 yield decrypted_chunk
 
+        return stream
+
+    def generate_stream(self, payload: PlayableContentFeeder.LoadedStream,
+                        bitrate: int, size: int) -> Generator[BytesIO, None, None]:
+        
+        def async_write(ffmpeg_process, stream):
+            try:
+                while True:
+                    in_chunk = stream.read(self.chunk_size)
+                    if not in_chunk:
+                        ffmpeg_process.stdin.close()
+                        stream.read(self.chunk_size)
+                        break
+                    ffmpeg_process.stdin.write(in_chunk)
+            except:
+                ffmpeg_process.stdin.close()
+                stream.close()
+
+        def stream():
+            input_stream = payload.input_stream.stream()
+            ffmpeg_process = subprocess.Popen(
+                'ffmpeg -f ogg -i - -vn -b:a {}k -f mp3 -'
+                .format(bitrate).split(),
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.DEVNULL
+            )
+            write_t = threading.Thread(target=async_write, args=(ffmpeg_process,input_stream,))
+            write_t.start()
+            transmitted = 0
+            try:
+                while True:
+                    out_chunk = ffmpeg_process.stdout.read(self.chunk_size)
+                    if not out_chunk:
+                        ffmpeg_process.stdout.close()
+                        ffmpeg_process.terminate()
+                        if transmitted < size:
+                            yield b'\0' * (size-transmitted)
+                        return
+                    transmitted += len(out_chunk)
+                    yield out_chunk
+            except:
+                ffmpeg_process.stdin.close()
+                ffmpeg_process.stdout.close()
+                ffmpeg_process.terminate()
         return stream
--- streamers/utils.py
+++ streamers/utils.py
@@ -7,3 +7,6 @@ def construct_url(path: str, scheme: str = 'http', **qargs):
     (_, netloc, _, _, _, _) = urlparse(flask.request.url_root)
     query_params = urlencode(qargs)
     return urlunparse((scheme,netloc, path, _, query_params, _))
+
+def estimate_size(duration: int, bitrate: int, offset: int = 20000) -> int:
+    return (duration * bitrate * 125) + offset
